73070fc00a7268d5cc91456a7677cb505803f967
Alex <o.a.momotyuk@gmail.com>
2019-08-26 12:39:34 -0500

    initial files

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const empireStateBuilding = {
+    stories: 103,
+    height: 1453,
+    address: "350 Fifth Avenue, Manhattan, New York 10118",
+    squareFeet: 2768591,
+    constructionDate: 1931,
+    cost: 40948900,
+    owner: "Empire State Realty Trust",
+    eastWestLength: 424,
+    northSouthLength: 187,
+    architect: "Shreve, Lamb & Harmon"
+}
+
+/*
+Lightning Exercises 1: Use dot notation to output all of the dimensions of the Empire State Building to the console.
+
+Lightning Exercises 2: Use square bracket notation to output the remaining 5 properties to the console. Create 5 variables first with the keys as their values. Use those variables to look up the values.
+*/
+
+console.log( "Empire State Building Height:", empireStateBuilding.height );
