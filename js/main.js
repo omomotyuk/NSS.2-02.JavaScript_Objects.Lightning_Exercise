3ded4246266a54ecf6263705c6bbbdd47218eec1
Alex <o.a.momotyuk@gmail.com>
2019-08-26 14:12:40 -0500

    exercise is done

--- js/main.js
+++ js/main.js
@@ -11,19 +11,20 @@ const empireStateBuilding = {
     architect: "Shreve, Lamb & Harmon"
 }
 
+
 /*
 Lightning Exercises 1: Use dot notation to output all of the dimensions of the Empire State Building to the console.
+*/
+console.log( "Empire State Building Height:", empireStateBuilding.height );
+console.log( "Empire State Building Square Feet:", empireStateBuilding.squareFeet );
 
+
+/*
 Lightning Exercises 2: Use square bracket notation to output the remaining 5 properties to the console. Create 5 variables first with the keys as their values. Use those variables to look up the values.
 */
-
-console.log( "Empire State Building Height:", empireStateBuilding.height );
-console.log( "Empire State Building Square Feet:", empireStateBuilding.hsquareFeet );
-
 const properties = [ "stories", "address", "constructionDate", "cost", "owner", "eastWestLength", "northSouthLength", "architect" ];
 
 for ( var i = 0; i < properties.length; i++ ) {
-    const item = properties[i];
-    console.log( "Empire State Building ", properties[i], ":", empireStateBuilding["properties[i]"] );
+    console.log( "Empire State Building ", properties[i], ":", empireStateBuilding[ properties[i] ] );
 }
 
